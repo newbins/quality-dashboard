d493c56a169e58d3b7adec1c2ff280aedcdd7edf
Joshua Newbins <jnewbins@gmail.com>
2021-11-08 21:08:08 -0600

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center",
+        height: "100vh",
+        font: "roboto",
+      }}
+    >
+      <h1
+        style={{
+          color: "#6EC177",
+          margin: 0,
+        }}
+      >
+        Wah Gwan, Soon Come
+      </h1>
+      <p> What's Up, Coming Soon</p>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/firebase.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAGhAITEW4ufhiPJPPWyxlWEU5DNxfJf7M",
+  authDomain: "quality-dashboard-produc-5bdf8.firebaseapp.com",
+  databaseURL:
+    "https://quality-dashboard-produc-5bdf8-default-rtdb.firebaseio.com",
+  projectId: "quality-dashboard-produc-5bdf8",
+  storageBucket: "quality-dashboard-produc-5bdf8.appspot.com",
+  messagingSenderId: "272005919562",
+  appId: "1:272005919562:web:beccb81cd69181de7a47b3",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
