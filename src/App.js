71b4e4f2b6b5ca1e14b238d7d522dbaeaa091b5d
Joshua Newbins <jnewbins@gmail.com>
2021-11-09 18:59:43 -0600

    Navigation

--- src/App.js
+++ src/App.js
@@ -1,30 +1,32 @@
-import logo from "./logo.svg";
-import "./App.css";
+import React from "react";
+import Navigation from "./components/Navigation";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import routes from "./routes";
+import { useStyles } from "./styles";
 
-function App() {
+const App = () => {
+  const classes = useStyles();
   return (
-    <div
-      style={{
-        display: "flex",
-        flexDirection: "column",
-        justifyContent: "center",
-        alignItems: "center",
-        height: "100vh",
-        font: "roboto",
-      }}
-    >
-      <img src={logo} alt="Quality Logo" />
-      <h1
-        style={{
-          color: "#6EC177",
-          margin: 0,
-        }}
-      >
-        Wah Gwan, Soon Come!
-      </h1>
-      <p> What's Up, Coming Soon!</p>
+    <div className={classes.appRoot}>
+      <Router>
+        <Navigation />
+        <div>
+          <Routes>
+            {routes.map((route, index) => {
+              return (
+                <Route
+                  exact
+                  key={index}
+                  path={route.path}
+                  element={route.element}
+                ></Route>
+              );
+            })}
+          </Routes>
+        </div>
+      </Router>
     </div>
   );
-}
+};
 
 export default App;
